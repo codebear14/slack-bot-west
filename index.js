17f6764a39f17683ae91decb1565835608c97a83
SoundaryaKorlapati <soundkoral96@gmail.com>
2019-12-23 00:02:07 -0600

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const SlackBot = require('slackbots')
 const axios = require('axios')
+
+const bot = new SlackBot({
+    token: 'xoxb-883833217717-885722925607-2nxHIbDkgCsVBreTWo7qtjMP',
+    name: 'westbot'
+})
