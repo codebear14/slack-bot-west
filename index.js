f64354276f0cdd7efccf514a8f86862765857262
SoundaryaKorlapati <soundkoral96@gmail.com>
2019-12-23 19:52:24 -0600

    Comitting

--- index.js
+++ index.js
@@ -6,4 +6,54 @@ const bot = new SlackBot({
     name: 'westbot'
 })
 
-//Task Begins
+//Sound's part
+
+bot.on('start', () => {
+    const params = {
+        icon_emoji: ':alien:'
+    }
+
+    bot.postMessageToChannel(
+        'general', 
+        '*Hey, I\'m Online!!*', 
+        params 
+    )
+})
+
+
+// Error Handler
+
+bot.on('error', (err) => console.log(err));
+
+// Message Handler
+
+bot.on('message', (data) => {
+    if(data.type !== 'message'){
+        return
+    }
+
+    handleMessage(data.text)
+})
+
+function handleMessage(message){
+    if(message.includes(' chucknorris')){
+        chuckjoke()
+    }
+}
+
+function chuckjoke(){
+    axios.get('http://api.icndb.com/jokes/random')
+    .then(res => {
+        const joke = res.data.value.joke
+        //sending the joke to slack
+        const params = {
+            icon_emoji: ':laughing:'
+        }
+    
+        bot.postMessageToChannel(
+            'general', 
+            `Chuck Norris: ${joke}`, 
+            params 
+        )
+    })
+}
