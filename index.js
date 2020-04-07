9bf752cd09b6462aad21eedb3990e438f838e6d4
satyam.ptodkar <59157719+codebear14@users.noreply.github.com>
2020-04-07 14:01:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const SlackBot = require('slackbots')
 const axios = require('axios')
 
 const bot = new SlackBot({
-    token: 'xoxb-883833217717-885722925607-2nxHIbDkgCsVBreTWo7qtjMP',
+    token: 'SLACK TOKEN',
     name: 'westbot'
 })
 
